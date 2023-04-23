2464c6ec95e29fba072191baad027aab98c90dd1
Mberndt92 <maximilian_berndt@icloud.com>
2023-04-24 08:05:36 +0900

    Initial Commit

---
+++ iMeetUp/iMeetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMeetUpApp.swift
+//  iMeetUp
+//
+//  Created by Maximilian Berndt on 2023/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iMeetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
